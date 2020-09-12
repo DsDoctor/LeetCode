921e10427d0d502d6508c82e47ff6dd65a2ae872
dsdoctor <ds_doctor@foxmail.com>
2020-09-12 19:41:05 +0800

    40ms  17.8MB

--- offer04.py
+++ offer04.py
@@ -1,6 +1,7 @@
 def findNumberIn2DArray(matrix, target):
     """
-    在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+    在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+    请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 
     示例:
     现有矩阵 matrix 如下：
@@ -11,8 +12,8 @@ def findNumberIn2DArray(matrix, target):
       [10, 13, 14, 17, 24],
       [18, 21, 23, 26, 30]
     ]
-    给定 target = 5，返回 true。
-    给定 target = 20，返回 false。
+    给定 target=5，返回true。
+    给定target=20，返回false。
 
     来源：力扣（LeetCode）
     链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
