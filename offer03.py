d189d184d2da4f9088b9bbfe574a341ffdc8b81b
dsdoctor <ds_doctor@foxmail.com>
2020-09-12 19:18:55 +0800

    36ms  23.1MB

--- offer03.py
+++ offer03.py
@@ -1,7 +1,8 @@
 def findRepeatNumber(nums):
     """
     找出数组中重复的数字。
-    在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+    在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
+    数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
 
     示例 1：
     输入：
@@ -12,12 +13,12 @@ def findRepeatNumber(nums):
     链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
     """
-    dic = {}
+    dic = set()
     for n in nums:
-        if dic.get(n):
+        if n in dic:
             return n
         else:
-            dic[n] = True
+            dic.add(n)
 
 
 if __name__ == '__main__':
